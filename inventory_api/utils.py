839884b9f18cfbf28256909a75c32951aea189cc
Chris Rodriguez <rodcee13@gmail.com>
2022-11-20 11:45:36 -0500

    More authentication

--- inventory_api/utils.py
+++ inventory_api/utils.py
@@ -1,6 +1,7 @@
 import jwt
 from datetime import datetime, timedelta
 from django.conf import settings
+from user_control.models import CustomUser
 
 
 def get_access_token(payload, expiry):
@@ -11,3 +12,22 @@ def get_access_token(payload, expiry):
     )
     return token
 
+
+def decodeJWT(bearer):
+    if not bearer:
+        return None
+    
+    token = bearer[7:]
+
+    try:
+        decoded = jwt.decode(
+            token, key=settings.SECRET_KEY, algorithm="HS256"
+        )
+    except Exception:
+        return None
+    
+    if decoded:
+        try:
+            return CustomUser.objects.get(id=decoded["user_id"])
+        except Exception:
+            return None
